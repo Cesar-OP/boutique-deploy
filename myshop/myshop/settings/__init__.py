9ad7f306889f0119fef38f8b794ffadf12c2b8db
Cesar <adm.anasantosjesus@gmail.com>
2023-10-01 18:37:30 -0300

    Fix settings path for Heroku

--- myshop/myshop/settings/__init__.py
+++ myshop/myshop/settings/__init__.py
@@ -1,7 +1,3 @@
 from .base import *
 from .prod import *
 
-try:
-    from .local import *
-except:
-    pass
--- myshop/myshop/wsgi.py
+++ myshop/myshop/wsgi.py
@@ -11,6 +11,7 @@ import os
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings.prod')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root='static')
