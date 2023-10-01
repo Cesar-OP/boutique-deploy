3b3520c744b9a75724c525f78a9fe046dc89b89c
Cesar <adm.anasantosjesus@gmail.com>
2023-10-01 18:13:42 -0300

    wsgi file

--- myshop/myshop/wsgi.py
+++ myshop/myshop/wsgi.py
@@ -11,8 +11,7 @@ import os
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/path/to/static/files')
+application = WhiteNoise(application, root='static')
 
